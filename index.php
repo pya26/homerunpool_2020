4fbbfadeb71b8f34c3cdc9530efa7111ddccf9e8
Kevin Phillips <kphillips@entnet.org>
2020-01-27 13:26:18 -0500

    add accept cookies footer/banner

--- index.php
+++ index.php
@@ -248,5 +248,18 @@
 </footer>
 <!-- Footer -->
 
+
+<!-- START Bootstrap-Cookie-Alert -->
+<div class="alert text-center cookiealert" role="alert">
+    <b>Do you like cookies?</b> &#x1F36A; We use cookies to ensure you get the best experience on our website. <a href="https://cookiesandyou.com/" target="_blank">Learn more</a>
+
+    <button type="button" class="btn btn-primary btn-sm acceptcookies" aria-label="Close">
+        I agree
+    </button>
+</div>
+
+<!-- Include cookiealert script -->
+<script src="https://cdn.jsdelivr.net/gh/Wruczek/Bootstrap-Cookie-Alert@gh-pages/cookiealert.js"></script>
+
   </body>
 </html>
