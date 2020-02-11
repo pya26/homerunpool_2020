88446954b6d0550f4e73a7e0d357722a520f49c4
Kevin Phillips <kphillips@entnet.org>
2020-02-11 16:48:43 -0500

    added leaderboard and team tables on homepage

---
+++ team_table.php
@@ -0,0 +1,92 @@
+<div class="container table-responsive-sm">
+    <h2>Team Name</h2>
+<!--
+	<span class="fa-stack fa-2x">
+		<i class="fa fa-trophy fa-stack-2x" style="color:#df691a;"></i>
+			<span class="fa fa-stack-1x" style="color:#fff;">
+				<span style="font-size:16px; margin-top:-20px; display:block;">
+					2009
+				</span>
+			</span>
+	</span>
+-->
+	  
+    <table id="team_table" class="table table-striped table-hover table-secondary table-sm" style="width:100%; border-color: #fff;">
+		<thead>
+			<tr>
+				<th>Player</th>
+				<th>Mar</th>
+				<th>Apr</th>
+				<th>May</th>
+				<th>June</th>
+				<th>July</th>
+				<th>Aug</th>
+				<th>Sept</th>
+				<th>Oct</th>
+				<th>Total</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Carl Yastrzemski</td>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>7</td>
+				<td>8</td>
+				<td>50</td>
+			</tr>
+			<tr>
+				<td>Fred Lynn</td>
+				<td>8</td>
+				<td>7</td>
+				<td>6</td>
+				<td>5</td>
+				<td>4</td>
+				<td>3</td>
+				<td>2</td>
+				<td>1</td>
+				<td>40</td>
+			</tr>
+			<tr>
+				<td>Dwight Evans</td>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>7</td>
+				<td>8</td>
+				<td>30</td>
+			</tr>
+			<tr>
+				<td>Jim Rice</td>
+				<td>8</td>
+				<td>7</td>
+				<td>6</td>
+				<td>5</td>
+				<td>4</td>
+				<td>3</td>
+				<td>2</td>
+				<td>1</td>
+				<td>20</td>
+			</tr>
+			<tr>
+				<td>Mike Greenwell</td>
+				<td>1</td>
+				<td>2</td>
+				<td>3</td>
+				<td>4</td>
+				<td>5</td>
+				<td>6</td>
+				<td>7</td>
+				<td>8</td>
+				<td>10</td>
+			</tr>
+		</tbody>
+    </table>
+</div>
