7364a4f3b6eeb0f1753824027150358a515ea0b0
Kevin Phillips <kphillips@entnet.org>
2020-03-14 09:05:48 -0400

    added updated database with neew priviledges to import into Bluehost, updated config file with new database name and new credentials

--- _config/config.php
+++ _config/config.php
@@ -13,9 +13,9 @@
 
 
 $GLOBALS['db_host'] = 'localhost';
-$GLOBALS['db_user'] = 'root';
-$GLOBALS['db_password'] = '';
-$GLOBALS['db_name'] = 'homerunpool';
+$GLOBALS['db_user'] = 'homeruo9_kevin';
+$GLOBALS['db_password'] = 'kelster26';
+$GLOBALS['db_name'] = 'homeruo9_homerunpool';
 $GLOBALS['msf_apikey_token'] = '8cba6612-b899-4bd5-9d83-2db6e3';
 $GLOBALS['msf_password'] = 'MYSPORTSFEEDS';
 $GLOBALS['msf_api_v2_base_url'] = 'https://api.mysportsfeeds.com/v2.1/pull/mlb/';
