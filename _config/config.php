5b51c5357f0fe43facb3d6d4b690fc9107c6aca8
Kevin Phillips <kphillips@entnet.org>
2020-03-18 18:34:33 -0400

    updated config file

--- _config/config.php
+++ _config/config.php
@@ -7,11 +7,22 @@
 	$GLOBALS['msf_apikey_token'] = '8cba6612-b899-4bd5-9d83-2db6e3';
 	$GLOBALS['msf_password'] = 'MYSPORTSFEEDS';
 	$GLOBALS['msf_api_v2_base_url'] = 'https://api.mysportsfeeds.com/v2.1/pull/mlb/';
-	$GLOBALS['base_url'] = 'http://localhost/sandbox/homerunpool_2020/';
-	$GLOBALS['base_path'] = '/sandbox/homerunpool_2020/';
+	//$GLOBALS['base_url'] = 'http://localhost/sandbox/homerunpool_2020/';
+	//$GLOBALS['base_path'] = '/sandbox/homerunpool_2020/';
 	$GLOBALS['config_base_path'] = 'C:\laragon\www\sandbox\homerunpool_2020';
 	$GLOBALS['league_id'] = 10;
 	$GLOBALS['season_id'] = 10;
+
+	if($_SERVER['CONTEXT_DOCUMENT_ROOT'] == 'C:/laragon/www'){
+		$GLOBALS['base_url'] = 'http://localhost/sandbox/homerunpool_2020/';
+		$GLOBALS['base_path'] = '/sandbox/homerunpool_2020/';
+	} else {
+	/**
+	* set variables for bluehost
+	*/
+		$GLOBALS['base_url'] = 'http://box2009.temp.domains/~homeruo9/';
+		$GLOBALS['base_path'] = '/home1/homeruo9/public_html';
+	}
 
 	$leagueid = $GLOBALS['league_id'];
 	$seasonid = $GLOBALS['season_id'];
