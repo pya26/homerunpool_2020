46b9bdf6488985765523c7577f3c126fca3450f1
Kevin Phillips <kphillips@entnet.org>
2022-05-25 11:21:50 -0400

    testing first commit (and push to remote) after installing sourcetree on new Surface Pro machine

--- scrap_paper.php
+++ scrap_paper.php
@@ -1,16 +1,36 @@
-<?php
-date_default_timezone_set('America/New_York');
-$timezone = date_default_timezone_get();
-echo "The current server timezone is: " . $timezone;
-
-$date = date('m/d/Y h:i:s a', time());
-print '<br />'. $date;
-
-
-
-
-
-
-
-
-?>
+<?php
+
+date_default_timezone_set('America/New_York');
+
+$timezone = date_default_timezone_get();
+
+echo "The current server timezone is: " . $timezone;
+
+
+
+$date = date('m/d/Y h:i:s a', time());
+
+print '<br />'. $date;
+
+print '<br />Just testing';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
